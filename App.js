5b2e30db53cbd251a5a70a6285dc1ae9c2cc07f2
Ana Antunes <ana.antun3s@gmail.com>
2024-05-08 15:26:02 -0300

    commit

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.nome}>Ana Julia Antunes Cardoso</Text>
+      <Text style={styles.numero}>03</Text>
+      <Text style={styles.sala}>I2HN</Text>
+      <Text style={styles.hora}>15:20</Text>
     </View>
   );
 }
@@ -13,8 +14,33 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
+  },
+  nome: {
+    color: "blue",
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "right",
+  },
+  numero: {
+    fontSize: 25,
+    fontFamily: "italic",
+    borderBottomWidth: 1,
+    borderBottomColor: "black",
+    textAlign: "left",
+    width: 50,
+  },
+  sala: {
+    fontSize: 20,
+    textAlign: "center",
+    justifyContent: "center",
+    backgroundColor: "pink",
+  },
+  hora: {
+    backgroundColor: "purple",
+    borderColor: "black",
+    borderWidth: 1,
+    textAlign: "center",
   },
 });
